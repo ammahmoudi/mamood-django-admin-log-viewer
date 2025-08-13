67cd63ca9c06523e631d81fbb775c7599f9a13e0
Amirhossein Mahmoudi <am.mahmoudi@outlook.com>
2025-08-13 17:35:36 +0330

    feat: Initialize __init__.py and update pyproject.toml for versioning and dependencies

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+Django Admin Log Viewer
+
+A Django app for viewing log files in the admin interface.
+"""
+
+__version__ = '2.0.0'
+
+default_app_config = 'django_admin_log_viewer.apps.DjangoAdminLogViewerConfig'
